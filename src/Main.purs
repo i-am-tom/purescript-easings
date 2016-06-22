d56d09ad4ad07294880c477a97c6c9e410f7dfed
Tom Harding <tomjharding@live.co.uk>
2016-06-22 08:43:04 +0100

    Removing Exponentiation Operator.
    Now there's only one use for it, it can be removed!

--- src/Main.purs
+++ src/Main.purs
@@ -3,26 +3,20 @@ module Main where
 import Prelude
 import Math (pi, sin, cos, pow)
 
-type Start    = Number
-type End      = Number
-type Progress = Number
-
-type Easing = Start -> End -> Progress -> Number
-
-infix 9 pow as ^
+type Easing = Number -> Number -> Number -> Number
 
 out :: Easing -> Easing
 out f start end progress = f end start (1.0 - progress)
 
 inAndOut :: Easing -> Easing
-inAndOut fIn start end progress = if progress < 0.5
-  then fIn start (end / 2.0) (progress * 2.0)
-  else (out fIn) ((start + end) / 2.0) end (2.0 * progress - 1.0)
+inAndOut f start end progress = if progress < 0.5
+  then f start (end / 2.0) (progress * 2.0)
+  else (out f) ((start + end) / 2.0) end (2.0 * progress - 1.0)
 
 -- Exponential easing.
 exponential :: Number -> Easing
 exponential power start end progress =
-  progress ^ power * (end - start) + start
+  (pow progress power) * (end - start) + start
 
 -- Sinusoidal easing.
 sine :: Easing
