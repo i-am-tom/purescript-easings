393e5922a19e0069afe929ac4d6567571c90d81d
Tom <tomjharding@live.co.uk>
2016-06-26 15:53:28 +0100

    Pattern Matching.
    The `if` statements have been replaced by guards and pattern
    matching. I just think it looks neater.

--- src/Main.purs
+++ src/Main.purs
@@ -1,7 +1,7 @@
 module Main where
 
 import Prelude
-import Math (cos, pi, pow, sin, sqrt)
+import Math (abs, cos, pi, pow, sin, sqrt)
 
 type Easing = Number -> Number -> Number -> Number
 
@@ -11,9 +11,9 @@ out :: Easing -> Easing
 out f start end progress = f end start (1.0 - progress)
 
 inAndOut :: Easing -> Easing
-inAndOut f start end progress = if progress < 0.5
-  then f start (end / 2.0) (progress * 2.0)
-  else (out f) ((start + end) / 2.0) end (2.0 * progress - 1.0)
+inAndOut f start end progress
+  | progress < 0.5 = f start (end / 2.0) (progress * 2.0)
+  | otherwise      = (out f) ((start + end) / 2.0) end (2.0 * progress - 1.0)
 
 -- Polynomial easing.
 polynomial :: Number -> Easing
@@ -28,9 +28,8 @@ sine start end = (+) end
 
 -- Exponential easing.
 exponential :: Easing
-exponential start end progress = if progress == 0.0
-  then start -- This is to squash the negligible result.
-  else start + (end - start) * 2.0 ^ (10.0 * (progress - 1.0))
+exponential start end 0.0 = start
+exponential start end progress = start + (end - start) * 2.0 ^ (10.0 * (progress - 1.0))
 
 -- Circular easing.
 circular :: Easing
