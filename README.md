8a90e64ebee24541beb285010feb188fda16dd29
Tom <tomjharding@live.co.uk>
2017-08-02 21:43:05 +0100

    Update package for release
    More documentation. Pulp instead of psc-package. This champ is ready to
    go.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # PureScript Easings [![Build Status](https://travis-ci.org/i-am-tom/purescript-easings.svg?branch=master)](https://travis-ci.org/i-am-tom/purescript-easings)
 
-The standard set of easing functions implemented in PureScript. Adapted from [jQuery Easing](https://github.com/gdsmith/jquery.easing), which was most likely stolen from Robert Penner's [Programming Macromedia Flash MX](https://www.amazon.com/Robert-Penners-Programming-Macromedia-Flash/dp/0072223561/175-5593647-3662611?ie=UTF8&tag=stackoverfl08-20).
+The standard set of easing functions implemented in PureScript. Adapted from [jQuery Easing](https://github.com/gdsmith/jquery.easing), which was most likely stolen from Robert Penner's [Programming Macromedia Flash MX](https://www.amazon.com/Robert-Penners-Programming-Macromedia-Flash/dp/0072223561/175-5593647-3662611?ie=UTF8&tag=stackoverfl08-20). There are no new ideas in programming.
 
-Unlike the jQuery library, only the ease-in functions are explicitly given. In lieu of the others, two functions, `out` and `inAndOut` have been written, which transform the other easing functions appropriately.
+Unlike the jQuery library, only the ease-in functions are explicitly given. To keep things Dynamic™ and Maintanable™, `out` and `inAndOut` exist, which transform the other easing functions into out-easing and symmetrically-easing functions, respectively. You want elastic easing on the way out? `out elastic` is the function for you! Want to bounce off the walls? `inAndOut bounce` just might make your day.
 
 ## API
 
@@ -18,7 +18,7 @@ The `Easing` type is `Start -> End -> Progress -> Result`, where all four are `N
 | `back` | `Easing` | Pulls back before transition. |
 | `bounce` | `Easing` | `in`/`out` are reversed. |
 
-All of these can be seen on [easings.net](http://www.easings.net), though do note that `bounce` has been flipped. This could change in the future, though I think it makes more sense to be this way round. Go figure.
+All of these can be seen on [easings.net](http://www.easings.net), though do note that `bounce` has been flipped. You'll see why when you look at the demo; what bounces on the way _in_? It's pretty weird, friend.
 
 ## Contributing
 
--- src/Main.purs
+++ src/Main.purs
@@ -1,47 +1,84 @@
-module Main where
+module Main
+  ( out
+  , inAndOut
+
+  , polynomial
+  , sine
+  , exponential
+  , circular
+  , elastic
+  , back
+  , bounce
+  ) where
 
 import Prelude
 import Math (cos, pi, pow, sin, sqrt)
 
-type Easing = Number -- Starting value
-           -> Number -- Ending value
-           -> Number -- 0-1 progress value
-           -> Number -- Result
+-- | The `Easing` type is just to make the code a bit easier to read.
+-- | We can call the three inputs, "start", "end", and "progress",
+-- | respectively. The "progress" is a number (usually) between 0 and
+-- | 1, indicating how far through the animation we are (0 being the
+-- | start, and 1 being the end). The "start" and "end" values are the
+-- | actual values between which we're easing. The result is simply
+-- | what the value would be at that point in the progress!
+type Easing = Number -> Number -> Number -> Number
 
 infixl 9 pow as ^
 
--- Convert an in-easing to an out-easing.
+-- | All the easings "ease in". For example, the elastic easing will
+-- | be elastic around the _end_ of the animation, rather than the
+-- | beginning. Sometimes, however, you want the opposite. Well, as
+-- | luck would have it, we have the function for you! Simply, this
+-- | flips the inputs and progress for an easing function, so you can
+-- | use `out circular` just as you'd use `circular`.
 out :: Easing -> Easing
 out f start end progress = f end start (1.0 - progress)
 
--- Convert an in-easing to an in-and-out easing.
+-- | With all these in and out escapades, you might find yourself with
+-- | a longing - a _yearning_ - for symmetry. Be afflicted no more:
+-- | `inAndOut` will take a function, and mirror it, such that it will
+-- | ease in and out in exactly the same way.
 inAndOut :: Easing -> Easing
 inAndOut f start end progress
   | progress < 0.5 = f start (end / 2.0) (progress * 2.0)
   | otherwise      = (out f) ((start + end) / 2.0) end (2.0 * progress - 1.0)
 
--- Polynomial easing (given a power).
+-- | Polynomial easing. `polynomial 1` is your run-of-the-mill linear
+-- | easing, start-to-end moving at a constant speed, yawn. However,
+-- | the 1 can be replaced with other numbers, and that value will be
+-- | the power to which `progress` is lifted. As it goes up, entry and
+-- | exit will become steeper. Somewhere between 1 and 5 is usually
+-- | pretty good.
 polynomial :: Number -> Easing
 polynomial power start end progress =
   progress ^ power * (end - start) + start
 
--- Sinusoidal easing.
+-- | Sinusoidal easing. This is an easing that follows a sinusoidal
+-- | curve from `start` to `end`, starting fast and ending slow. Bit
+-- | of trivia: internally, it uses `cos`, not `sin`. Pranked ya!
 sine :: Easing
 sine start end progress = end + (start - end) * delta
   where delta = cos $ progress * (pi / 2.0)
 
--- Exponential easing.
+-- | Exponential easing. This is an easing whose speed follows the
+-- | exponential curve: it starts very slow, and ends very fast.
 exponential :: Easing
 exponential start end 0.0      = start
 exponential start end progress = start + (end - start) * delta
   where delta = 2.0 ^ (10.0 * (progress - 1.0))
 
--- Circular easing.
+-- | Circular easing. Why settle for things like sinusoidal easing
+-- | when you can follow the exact edge of a circle? Look, reader: I
+-- | ported this library from a jQuery plugin. I didn't get to make
+-- | any decisions about its content.
 circular :: Easing
 circular start end progress = start + (start - end) * delta
   where delta = (sqrt $ 1.0 - progress ^ 2.0) - 1.0
 
--- Elastic easing.
+-- | Elastic easing. This is the sexy animation that everyone wants to
+-- | see on your Web2.0 startup page. The animation quickly moves
+-- | _beyond_ the final value, and then elastically bounces back to
+-- | the value you actually wanted. Watch your investors *swoon*.
 elastic :: Easing
 elastic start _ 0.0 = start
 elastic _ end 1.0 = end
@@ -49,15 +86,30 @@ elastic start end progress =
   (start - end) * 2.0 ^ (10.0 * (progress - 1.0))
     * (sin $ pi * (2.0 * progress - 1.85) / 0.15)
 
--- Back easing.
+-- | Back easing. I'd have called this one "run-up": we move slightly
+-- | backwards from the start, then charge forward to the end, with a
+-- | little pinch of acceleration for good measure. Think about blinds
+-- | in cartoons.
 back :: Easing
 back start end progress = start + (end - start)
   * progress ^ 2.0 * (2.70158 * progress - 1.70158)
 
--- Bounce easing.
+-- | Imagine your "end" value is a solid wall. This basically looks
+-- | like `elastic`, except it "bounces" off the end value instead of
+-- | going over it. Imagine dropping an `h1` from 200px high onto a
+-- | tarmac floor (remembering that `h1`s are quite rubbery). That's
+-- | what you get with `bounce -200.0 0`. This isn't the prettiest of
+-- | implementations, so maybe just don't look at it for too long.
 bounce :: Easing
 bounce start end progress
-  | progress < 1.0 / 2.75 = start + (end - start) * 7.5625 * progress ^ 2.0
-  | progress < 2.0 / 2.75 = start + (end - start) * (7.5625 * (progress - 1.5 / 2.75) ^ 2.0 + 0.75)
-  | progress < 2.5 / 2.75 = start + (end - start) * (7.5625 * (progress - 2.25 / 2.75) ^ 2.0 + 0.9375)
-  | otherwise = start + (end - start) * (7.5625 * (progress - 2.625 / 2.75) ^ 2.0 + 0.984375)
+  | progress < 1.0 / 2.75 = start + (end - start)
+                          * 7.5625 * progress ^ 2.0
+
+  | progress < 2.0 / 2.75 = start + (end - start)
+                          * (7.5625 * (progress - 1.5 / 2.75) ^ 2.0 + 0.75)
+
+  | progress < 2.5 / 2.75 = start + (end - start)
+                          * (7.5625 * (progress - 2.25 / 2.75) ^ 2.0 + 0.9375)
+
+  | otherwise = start + (end - start)
+              * (7.5625 * (progress - 2.625 / 2.75) ^ 2.0 + 0.984375)
